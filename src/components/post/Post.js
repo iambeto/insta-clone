47c14926adffeb0436b7bc94bab974e75a45863a
iambeto <rscabrera@gmail.com>
2022-05-10 14:14:53 -0500

    added post.js and post.css

---
+++ src/components/post/Post.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Post.css";
+import { Avatar } from "@material-ui/core";
+
+function Post([username, caption, imageUrl ]) {
+    return (
+        <div className="post">
+            <div className="post__header">
+                {/* Header: avatar with user */}
+                <Avatar className="post__avatar" alt={username} src="/static/images/avatar/1.jpg" />
+                <h3>{username}</h3>
+            </div>
+            {/* image */}
+            <image className="post__image" src={imageUrl} alt="" />
+            {/* username + caption */}
+            <h4 className="post__text">
+                <strong>{username}</strong> {caption}
+            </h4>
+        </div>
+    );
+}
+
+export default Post;
